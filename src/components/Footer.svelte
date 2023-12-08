426f6ec6472dbc96c6a4d3c71594f81f820d2b79
roman <roman.moreno226@live.com>
2023-12-07 18:22:16 -0600

    Update personal website, added layout for astro

---
+++ src/components/Footer.svelte
@@ -0,0 +1,9 @@
+<div class="min-h-full">
+  <nav class="bg-[#000000]">
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-4">
+      <div class="flex h-16 items-center justify-between">
+        <!--remove this and the bar disapears-->
+      </div>
+    </div>
+  </nav>
+</div>
---
+++ src/components/Header.svelte
@@ -0,0 +1,22 @@
+<div class="min-h-full">
+  <nav class="bg-[#0284c7]">
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div class="flex h-16 items-center justify-between">
+        <div class="flex items-center">
+          <div class="hidden md:block">
+            <div class="ml-4 flex items-baseline space-x-4">
+              <p>Roman Moreno Jr</p>
+            </div>
+          </div>
+        </div>
+        <div class="hidden md:block">
+          <div class="ml-4 flex items-baseline space-x-10 md:ml-6">
+            <img src="/icons/linkedin_icon.png" alt="LinkedIn Icon" />
+            <img src="/icons/github_icon.png" alt="GitHub Icon" />
+            <img src="/icons/mail_icon.png" alt="Mail Icon" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</div>
