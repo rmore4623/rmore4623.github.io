2b64c2171f5535fe19df8bda7e83fd755c341f81
roman <roman.moreno226@live.com>
2024-01-16 09:16:31 -0600

    Add new icons and update components

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,31 +1,53 @@
 <script lang="ts">
-  function linkedinClick() {
-    console.log("LinkedIn Clicked");
-    window.open("https://www.google.com", "_blank");
+  import { onMount, afterUpdate } from "svelte";
+
+  let name = "Roman Moreno Jr";
+  let isScrolled = false;
+
+  onMount(() => {
+    window.addEventListener("scroll", handleScroll);
+  });
+
+  afterUpdate(() => {
+    if (isScrolled) {
+      window.scrollTo(0, window.scrollY + 50);
+    }
+  });
+
+  function handleScroll() {
+    console.log(window.scrollY);
+    isScrolled = window.scrollY > 0;
+  }
+
+  function handleClickLinkedIn() {
+    console.log("LinkedIn button clicked");
+    window.open("https://www.linkedin.com/in/roman-moreno-jr/", "_blank");
+  }
+
+  function handleClickGithub() {
+    console.log("Github button clicked");
+  }
+
+  function handleClickEmail() {
+    console.log("Email button clicked");
   }
 </script>
 
-<div class="min-h-full">
-  <nav class="bg-[#0284c7]">
-    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-      <div class="flex h-16 items-center justify-between">
-        <div class="flex items-center">
-          <div class="hidden md:block">
-            <div class="ml-4 flex items-baseline space-x-4">
-              <p>Roman Moreno Jr</p>
-            </div>
-          </div>
-        </div>
-        <div class="hidden md:block">
-          <div class="ml-4 flex items-baseline space-x-10 md:ml-6">
-            <button type="button" on:click={linkedinClick}>
-              <img src="/icons/linkedin_icon.png" alt="LinkedIn Icon" />
-            </button>
-            <img src="/icons/github_icon.png" alt="GitHub Icon" />
-            <img src="/icons/mail_icon.png" alt="Mail Icon" />
-          </div>
-        </div>
-      </div>
-    </div>
-  </nav>
-</div>
+
+<header
+  class="flex items-center justify-between bg-blue-500 text-white text-6xl font-extrabold p-5"
+  style:position={isScrolled ? "fixed" : "static"}
+>
+  <p class="mr-auto">{name}</p>
+  <div class="flex">
+    <button on:click={handleClickLinkedIn}>
+      <img src ="/icons/linkedin_icon.png" alt="Twitter" class="w-12 h-12"/>
+    </button>
+    <button on:click={handleClickGithub}>
+      <img src="/icons/github_icon.png" alt="Github" class="w-12 h-12"/>
+    </button>
+    <button on:click={handleClickEmail}>
+      <img src="/icons/mail_icon.png" alt="Email" class="w-12 h-12"/>
+    </button>
+  </div>
+</header>
---
+++ src/components/Projects.svelte
@@ -0,0 +1,33 @@
+<script>
+    let images = [
+      { src: 'icons/MediaNaranja.svg', description: 'Media Naranja Speed Dating' },
+      { src: 'icons/spotify.png', description: 'Spotify Widget' },
+      { src: 'icons/discord.png', description: 'Lucien Bot' },
+    ];
+
+    function handleClickLinkedIn() {
+        console.log("LinkedIn button clicked");
+        window.open("https://www.linkedin.com/in/roman-moreno-jr/", "_blank");
+    }
+</script>
+
+<div class="image-container">
+    {#each images as { src, description }}
+        <div class="flex justify-center m-10">
+            <img src={src} alt={description} class="image" height="96" width="96">
+            <p>{description}</p>
+        </div>
+    {/each}
+</div>
+  
+<style>
+    .image-container {
+        display: flex;
+        overflow-x: auto;
+        gap: 20px; /* Adjust the gap between images */
+    }
+    
+    .image {
+        max-width: 100%; /* Ensure images don't exceed container width */
+    }
+    </style>
