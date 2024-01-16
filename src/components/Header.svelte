1f0ebf890025664db782790a3f5aced2870da115
rmore4623 <roman.moreno226@live.com>
2024-01-16 09:51:30 -0600

    Removed layout component

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,31 +1,14 @@
 <script lang="ts">
-  import { onMount, afterUpdate } from "svelte";
-
   let name = "Roman Moreno Jr";
-  let isScrolled = false;
-
-  onMount(() => {
-    window.addEventListener("scroll", handleScroll);
-  });
-
-  afterUpdate(() => {
-    if (isScrolled) {
-      window.scrollTo(0, window.scrollY + 50);
-    }
-  });
-
-  function handleScroll() {
-    console.log(window.scrollY);
-    isScrolled = window.scrollY > 0;
-  }
 
   function handleClickLinkedIn() {
     console.log("LinkedIn button clicked");
-    window.open("https://www.linkedin.com/in/roman-moreno-jr/", "_blank");
+    window.open("https://www.linkedin.com/in/romanmorenojr/", "_blank");
   }
 
   function handleClickGithub() {
     console.log("Github button clicked");
+    window.open("https://github.com/rmore4623", "_blank");
   }
 
   function handleClickEmail() {
@@ -36,18 +19,17 @@
 
 <header
   class="flex items-center justify-between bg-blue-500 text-white text-6xl font-extrabold p-5"
-  style:position={isScrolled ? "fixed" : "static"}
 >
   <p class="mr-auto">{name}</p>
   <div class="flex">
     <button on:click={handleClickLinkedIn}>
-      <img src ="/icons/linkedin_icon.png" alt="Twitter" class="w-12 h-12"/>
+      <img src ="/icons/linkedin_icon.png" alt="Twitter" class="w-12 h-12 mr-10"/>
     </button>
     <button on:click={handleClickGithub}>
-      <img src="/icons/github_icon.png" alt="Github" class="w-12 h-12"/>
+      <img src="/icons/github_icon.png" alt="Github" class="w-12 h-12 mr-10"/>
     </button>
     <button on:click={handleClickEmail}>
-      <img src="/icons/mail_icon.png" alt="Email" class="w-12 h-12"/>
+      <img src="/icons/mail_icon.png" alt="Email" class="w-12 h-12 mr-10"/>
     </button>
   </div>
 </header>
