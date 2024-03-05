426dd1b7e86a776b8399790b28c1756e4e27c258
roman <roman.moreno226@live.com>
2024-03-04 23:11:22 -0600

    Add new files and update website layout

---
+++ src/components/MainPage.svelte
@@ -0,0 +1,67 @@
+<script>
+    function downloadResume() {
+      const a = document.createElement('a');
+
+      a.href = '/files/Resume.pdf';
+      a.download = 'romans_resume.pdf';
+      a.click();
+    }
+
+    function scrollTo(event, sectionId) {
+      event.preventDefault();
+      const section = document.getElementById(sectionId);
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+</script>
+  
+  <div class="box-border flex relative flex-col shrink-0 w-screen bg-orange-100 ml-[calc(50%_-_50vw)]">
+    <header class="fixed-height">
+    <nav class="box-border flex sticky flex-col shrink-0 w-screen h-screen ml-[calc(50%_-_50vw)]">
+      <div class="flex gap-5 max-md:flex-col max-md:gap-0 max-md:">
+        <div class="flex flex-col w-6/12 max-md:ml-0 max-md:w-full">
+          <section class="box-border flex relative flex-col shrink-0 my-auto h-screen bg-white">
+            <div class="box-border flex relative flex-col shrink-0 grow-0 h-[100px]">
+              <div class="box-border flex relative flex-col shrink-0 m-auto">
+                <div class="flex gap-5 max-md:flex-col max-md:gap-0 max-md:">
+                  <div class="flex flex-col w-[25%] max-md:ml-0 max-md:w-full">
+                    <a class="box-border relative shrink-0 px-6 py-4 my-2.5 text-center rounded appearance-none cursor-pointer text-[black]" data-el="button-1" tabindex="0" href="#about" on:click={(e) => scrollTo(e, 'about')}>about</a>
+                  </div>
+                  <div class="flex flex-col ml-5 w-[25%] max-md:ml-0 max-md:w-full">
+                    <a class="box-border relative shrink-0 px-6 py-4 my-2.5 text-center rounded appearance-none cursor-pointer text-[black]" data-el="button-2" tabindex="0" href="#resume" on:click={(e) => scrollTo(e, 'resume')}>resume</a>
+                  </div>
+                  <div class="flex flex-col ml-5 w-[25%] max-md:ml-0 max-md:w-full">
+                    <a class="box-border relative shrink-0 px-6 py-4 my-2.5 text-center rounded appearance-none cursor-pointer text-[black]" data-el="button-3" tabindex="0" href="#projects" on:click={(e) => scrollTo(e, 'projects')}>projects</a>
+                  </div>
+                  <div class="flex flex-col ml-5 w-[25%] max-md:ml-0 max-md:w-full">
+                    <a class="box-border relative shrink-0 px-6 py-4 my-2.5 text-center rounded appearance-none cursor-pointer text-[black]" data-el="button-4" tabindex="0" href="#contact" on:click={(e) => scrollTo(e, 'contact')}>contact</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="box-border relative py-2.5 m-auto h-auto">
+              <h1 class="text-8xl font-bold">Roman Moreno Jr</h1>
+              <h2 class="text-4xl font-normal">Aspiring Software Developer</h2>
+            </div>
+          </section>
+        </div>
+        <aside class="flex flex-col mr-5 w-6/12 max-md:ml-0 max-md:w-full">
+          <button class="box-border relative shrink-0 self-center px-6 py-4 mt-5 mr-5 ml-auto w-auto text-center rounded appearance-none cursor-pointer bg-[black] text-[white]" data-el="button-4" tabindex="0" on:click={downloadResume}>Download Resume</button>
+          <img loading="lazy" src="/files/placeholder.png" alt="A Cool Pic" class="box-border object-cover overflow-hidden shrink-0 m-auto w-full aspect-[1.42] min-h-[20px] min-w-[20px"/>
+        </aside>
+      </div>
+     </nav>
+  </header>
+  </div>
+
+  <section id="about" class="box-border flex relative flex-col shrink-0 my-auto h-screen bg-white">
+    <h1 class="self-center">This is the about section</h1>
+  </section>
+  <section id="resume" class="box-border flex relative flex-col shrink-0 my-auto h-screen bg-white">
+    <h1>This is the resume section</h1>
+  </section>
+  <section id="projects" class="box-border flex relative flex-col shrink-0 my-auto h-screen bg-white">
+    <h1>This is the projects section</h1>
+  </section>
+  <section id="contact" class="box-border flex relative flex-col shrink-0 my-auto h-screen bg-white">
+    <h1>This is the contact section</h1>
+  </section>
--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -1,33 +1,82 @@
 <script>
     let images = [
-      { src: 'icons/MediaNaranja.svg', description: 'Media Naranja Speed Dating' },
-      { src: 'icons/spotify.png', description: 'Spotify Widget' },
-      { src: 'icons/discord.png', description: 'Lucien Bot' },
-    ];
+      { src: 'icons/MediaNaranja.png', 
+        title: 'Media Naranja Speed Dating', 
+        description: `Speed dating web application that seamlessly combines React
+                      for the dynamic user interface and Supabase for a backend infrastructure.
+                      The heart of the system lies in the admin dashboard, offering administrators
+                      the power to effortlessly create and manage events. Leveraging Supabase\'s
+                      secure authentication, the application ensures airtight access control.
+                      Real-time updates keep administrators informed and in control. For participants,
+                      our backend utilizes Supabase\'s database to intelligently match individuals
+                      based on gender, creating pairs with unique session tokens. Participants can
+                      conveniently scan QR codes to connect with their matched pairs. The entire system '
+                      is designed with responsiveness in mind, ensuring a seamless experience 
+                      across various devices.`
+      },
+      //
+      { src: 'icons/spotify.png', 
+        title: 'Spotify Widget', 
+        description: `This application, built with React in the frontend and Go in the backend, 
+                      allows users to authenticate with Spotify, view their playlists, 
+                      and access information about artists, and genres based on their playlists.
+                      The backend interacts with both the frontend and the Spotify API, and it
+                      includes a SQLite server for storing user sessions.`
 
-    function handleClickLinkedIn() {
-        console.log("LinkedIn button clicked");
-        window.open("https://www.linkedin.com/in/roman-moreno-jr/", "_blank");
-    }
+      },
+      //
+      { src: 'icons/discord.png', 
+        title: 'Lucien Bot', 
+        description: `Discord bot developed in python that demonstrates a simple 
+                      addition command that can add two numbers or concatenate two strings
+                      together, whether they are provided as digits or strings. This showcases
+                      how the bot can perform basic calculations. The bot uses the
+                      discord.ext.commands framework to handle user-defined commands easily!`
+      },
+    ]; 
+    
 </script>
 
-<div class="image-container">
-    {#each images as { src, description }}
-        <div class="flex justify-center m-10">
-            <img src={src} alt={description} class="image" height="96" width="96">
-            <p>{description}</p>
-        </div>
-    {/each}
-</div>
+<form>
+    <ul class="image-container snap-x snap-mandatory bg-white">
+      {#each images as { src, description, title }}
+        <li class="flex flex-col items-center justify-center m-10 shrink-0 snap-center">
+          <img src={src} alt={description} class="image" height="200" width="500">
+        </li>
+        <li class="flex flex-col items-center justify-center m-10 shrink-0 snap-center">
+            <h1 style="color:black" class="title">{title}</h1>
+            <p style="color:black" class="description">{description}</p>
+        </li>
+      {/each}
+    </ul>
+  </form>
+  
   
 <style>
     .image-container {
         display: flex;
         overflow-x: auto;
-        gap: 20px; /* Adjust the gap between images */
+        gap: 20px;
     }
+
+    .image-container li {
+    display: flex;
+    justify-content: center;
+    margin: 10px;
+    flex-grow: 0;
+    scroll-snap-align: center;
+    align-items: center;
+  }
     
     .image {
-        max-width: 100%; /* Ensure images don't exceed container width */
+        max-width: 100%;
+        object-fit: contain;
     }
-    </style>
+
+    .description {
+    font-size: 14px;
+    color: #333;
+    margin-top: 10px;
+    white-space: pre-line;
+  }
+</style>
